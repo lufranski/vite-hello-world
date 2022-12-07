00aa6e433a1b377a16e98d4382b3147a698271a7
lufranski <lufranski@gmail.com>
2022-12-07 16:22:05 +0100

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data(){
+        return{
+            img: 'hero.jpg'
+        };
+    },
+    methods: {
+        getImagePath: function (imgPath) {
+            return new URL(imgPath, import.meta.url).href;
+        }
+    }
+};
+</script>
+
+<template>
+<div>
+    <img :src="getImagePath(`../../public/${img}`)" alt="">
+</div>
+</template>
+
+<style scoped>
+img {
+    max-width: 90%;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    data(){
+        return{
+            saluto: 'La mia prima app con Vite!'
+        }
+    }
+}
+
+</script>
+
+<template>
+  <h1>{{ saluto }}</h1>
+
+  
+</template>
+
+<style scoped>
+h1 {
+  color: #181D31;
+  background-color: #678983;
+}
+</style>
